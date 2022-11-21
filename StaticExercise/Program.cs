1fdc13fc47d48859d11ce2a1af4d1c9ac77a7fca
Mark <mrivera8974@gmail.com>
2022-11-21 16:03:18 -0500

    part 1 complete

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var celsius = TempConverter.FahrenheitToCelsius(86);
+
+            Console.WriteLine($"{celsius} degrees Celsius");
+
+            Console.WriteLine();
+
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(30);
+
+            Console.WriteLine($"{fahrenheit} degrees Fahrenheit");
         }
     }
 }
